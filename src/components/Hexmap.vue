410f148492376f747962bfeb2b35df0124d8e8f1
Mikhail Gordin <mikhail.gordin@gmail.com>
2022-03-24 12:35:20 -0700

    Changed selected hex look

--- src/components/Hexmap.vue
+++ src/components/Hexmap.vue
@@ -9,12 +9,9 @@
 <template>
     <div class="container">
         <div class="image-row flat-row" v-for="row in hs.hexes">
-            <div class="image-hex flat-hex" v-for="hex in row" :key="hex.id" >
+            <div class="image-hex flat-hex" v-for="hex in row" :key="hex.id" v-bind:class="{'hex-selected': es.activeHexID == hex.id}" >
                 <img class="hex-terrain" v-bind="{ src: es.terrainToImage[hex.terrain].file }" @click="es.selectHex(hex)">
                 <div class="hex-overlay"></div>
-                <svg class="hex-border" viewbox="-4 0 110 96" v-bind:class="{'is-hidden': es.activeHexID != hex.id}">
-                    <polygon class="hex-border" points="28 3,75 3,100 50,75 97,28 97,0 50" stroke="white" fill="transparent" stroke-width="5"></polygon>
-                </svg>
             </div>
         </div>
     </div>
